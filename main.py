f28a909d8dd366d09440101ad693639de00605ea
Aditya Ranganath <aranganath@ucmerced.edu>
2023-07-29 19:59:46 -0700

    Script version for easy running

--- main.py
+++ main.py
@@ -1,58 +1,93 @@
-from models import TimeSeriesTransformer
-from dataset import TransformerDataset
+# %%
 from torch.utils.data import DataLoader
 from utils import *
+from matplotlib import pyplot
 import os
+from pdb import set_trace
+from dataset import TransformerDataset
 
+# %%
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+torch.set_default_device(device)
+print(device)
 
+# %%
 torch.set_default_dtype(torch.float64)
 # Okay, we kind of have a way of loading the data
 # We need to collect the data and feed it to the transformer model
 # Now how do we that ?
 
 
-# Right, so let's get the data
-
-
 # Torch Tensor data !
 path = './Datasets/intracardiac_dataset/'
-VmData, pECGData = fileReader(path)
+VmTrainData, pECGTrainData, VmDataTest, pECGTestData  = fileReader(path, 100)
 print('Data loading from files - complete')
-## Model parameters
-dim_val = 512 # This can be any value divisible by n_heads. 512 is used in the original transformer paper.
-n_heads = 8 # The number of attention heads (aka parallel attention layers). dim_val must be divisible by this number
-n_decoder_layers = 4 # Number of times the decoder layer is stacked in the decoder
-n_encoder_layers = 4 # Number of times the encoder layer is stacked in the encoder
-input_size = 75 # The number of input variables. 1 if univariate forecasting.
-dec_seq_len = 100 # length of input given to decoder. Can have any integer value.
-enc_seq_len = 200 # length of input given to encoder. Can have any integer value.
-output_sequence_length = 200 # Length of the target sequence, i.e. how many time steps should your forecast cover
-max_seq_len = enc_seq_len # What's the longest sequence the model will encounter? Used to make the positional encoder
-batch_size = 128
+
+VmTrainData = (VmTrainData - torch.min(VmTrainData))/(torch.max(VmTrainData) - torch.min(VmTrainData))
+pECGTrainData = (pECGTrainData - torch.min(pECGTrainData))/(torch.max(pECGTrainData) - torch.min(pECGTrainData))
+
+VmDataTest = (VmDataTest - torch.min(VmDataTest))/(torch.max(VmDataTest) - torch.min(VmDataTest))
+
+pECGTestData = (pECGTestData - torch.min(pECGTestData))/(torch.max(pECGTestData) - torch.min(pECGTestData))
+print('Normalization - complete!')
+
+# %%
+dim_val = 320
+n_heads = 32
+n_decoder_layers = 2
+n_encoder_layers = 2
+input_size = 12
+dec_seq_len = 498
+enc_seq_len = 500
+output_sequence_length = 75
+max_seq_len = enc_seq_len
+train_batch_size = 8
+test_batch_size = 10
 batch_first= False
+output_size = 75
+window_size = 75
+
+# %%
 
 # Get the indices of the sequences
 # The idea is: start - stop, where stop - start is window_size
 # This means, each tuple in VmInd and pECGInd is 50 steps
-VmInd, pECGInd = get_indices_entire_sequence(VmData = VmData, 
-                                            ECGData = pECGData, 
-                                            window_size= enc_seq_len + output_sequence_length, 
-                                            step_size = 1)
+datInd = get_indices_entire_sequence(VmData = VmTrainData, 
+                                            ECGData = pECGTrainData, 
+                                            window_size= window_size, 
+                                            step_size = window_size)
 
 # Now let's collect the training data in the Transformer Dataset class
-
-training_data = TransformerDataset(VmData = VmData, 
-                                    ECGData = pECGData,
-                                    VmIndices= VmInd,
-                                    ECGInd = pECGInd,
+TrainData = TransformerDataset(VmData = VmTrainData,
+                                    datInd=datInd,
+                                    ECGData = pECGTrainData,
                                     enc_seq_len = enc_seq_len,
                                     dec_seq_len = dec_seq_len,
-                                    target_seq_len = output_sequence_length)
+                                    target_seq_len = output_sequence_length
+                                )
 
 
-trainLoader = DataLoader(training_data, batch_size)
+TrainData = DataLoader(TrainData, train_batch_size)
+
+datInd = get_indices_entire_sequence(VmData = VmDataTest, 
+                                            ECGData = pECGTestData, 
+                                            window_size= window_size, 
+                                            step_size = window_size)
 
 
+# Now, let's load the test data
+TestData = TransformerDataset(VmData = VmDataTest, 
+                                    ECGData = pECGTestData,
+                                    datInd=datInd,
+                                    enc_seq_len = enc_seq_len,
+                                    dec_seq_len = dec_seq_len,
+                                    target_seq_len = output_sequence_length
+                                )
+
+TestData = DataLoader(TestData, test_batch_size)
+
+# %%
+from models import TimeSeriesTransformer
 model = TimeSeriesTransformer(
     dim_val=dim_val,
     batch_first=batch_first,
@@ -62,59 +97,88 @@ model = TimeSeriesTransformer(
     n_decoder_layers=n_decoder_layers,
     n_encoder_layers=n_encoder_layers,
     n_heads=n_heads,
-    num_predicted_features=input_size)
+    num_predicted_features=output_size
+)
+
+# model = torch.nn.DataParallel(model)
 
 # Define the MSE loss
-criterion = torch.nn.MSELoss()
+criterion = torch.nn.HuberLoss(delta=5.0)
 
 # Define the optimizer
 optimizer = torch.optim.Adam(model.parameters(), lr = 1e-3)
 
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10000, gamma=0.1)
 
-# Permute from shape [batch size, seq len, num features] to [seq len, batch size, num features]
-EPOCHS = 10
-src, trg, trg_y = next(iter(trainLoader))
+# %%
+EPOCHS= 80000
+train_losses = []
 src_mask = generate_square_subsequent_mask(
-            dim1=output_sequence_length,
-            dim2=enc_seq_len
-        )
+                dim1=output_sequence_length,
+                dim2=enc_seq_len
+            )
 tgt_mask = generate_square_subsequent_mask(
     dim1=output_sequence_length,
     dim2=output_sequence_length
 )
-src = src.permute(1, 0, 2)
-trg = trg.permute(1, 0, 2)
-prediction = model(
-src=src,
-tgt=trg,
-src_mask=src_mask,
-tgt_mask=tgt_mask
-)
 
-loss = criterion(trg_y , prediction.permute(1,0,2))
+gap_length = 1
+pbar = tqdm(range(EPOCHS), desc='Training')
 
-# Iterate over all epochs
-for epoch in tqdm(range(EPOCHS), desc = 'Training ', unit='epochs'):
-
-    for src, trg, trg_y in trainLoader:
+for epoch in pbar:
+    string = ''
+    for src, trg, trg_y in TrainData:
         optimizer.zero_grad()
-        src_mask = generate_square_subsequent_mask(
-            dim1=output_sequence_length,
-            dim2=enc_seq_len
-        )
-        tgt_mask = generate_square_subsequent_mask(
-            dim1=output_sequence_length,
-            dim2=output_sequence_length
-        )
-        src = src.permute(1, 0, 2)
-        trg = trg.permute(1, 0, 2)
         prediction = model(
-            src=src,
-            tgt=trg,
+            src=src.permute(1,0,2).to(device),
+            tgt=trg.permute(0,2,1,3).permute(1,0,2,3).to(device),
             src_mask=src_mask,
             tgt_mask=tgt_mask
-        )
-        loss = criterion(trg_y, prediction.permute(1,0,2))
-        print('Loss: ',loss.item())
+            )
+        
+        
+        loss = criterion(prediction.view_as(trg).to(device), trg_y.to(device))
         loss.backward()
         optimizer.step()
+        if (epoch+1) % gap_length == 0:
+            model.train = True
+            with torch.no_grad():
+                src, trg, trg_y = next(iter(TestData))
+                prediction = model(
+                        src=src.permute(1,0,2).to(device),
+                        tgt=trg.permute(0,2,1,3).permute(1,0,2,3).to(device),
+                        src_mask=src_mask,
+                        tgt_mask=tgt_mask
+                    )
+                row = 7
+                column = 10
+                
+
+                prediction = prediction.view_as(trg_y).reshape(-1,trg_y.shape[1]*trg_y.shape[2] , 75).detach().cpu()
+                trg_y = trg_y.reshape(-1, trg_y.shape[1]*trg_y.shape[2], 75).detach().cpu()
+                pyplot.figure(figsize=(18, 9))
+                for count, i in enumerate(range(prediction.shape[2])):
+                    pyplot.subplot(8, 10, count + 1)
+                    pyplot.plot(prediction[0,:,i])
+                    pyplot.plot(trg_y[0,:,i])
+                    pyplot.title(f'i = {i}')
+                    pyplot.grid(visible=True, which='major', color='#666666', linestyle='-')
+                    pyplot.minorticks_on()
+                    pyplot.grid(visible=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+                pyplot.tight_layout()
+                if not os.path.isdir('graphs'):
+                    os.mkdir('graphs')
+                pyplot.savefig('./graphs/model_'+str(dim_val) +'_encoder_'+ str(n_encoder_layers)+'_decoder_'+str(n_decoder_layers)+'_epochs_'+str(epoch)+'_window_size_'+str(window_size)+'.png')
+
+                PARENT_PATH = 'model_weights'
+                if not os.path.isdir(PARENT_PATH):
+                    os.mkdir(PARENT_PATH)
+                
+                PATH = './model_weights/model_'+str(dim_val) +'_encoder_'+ str(n_encoder_layers)+'_decoder_'+str(n_decoder_layers)+'_epochs_'+str(EPOCHS)+'.pth'
+                sring = 'Saved to path: '+PATH
+                torch.save(model.state_dict(), PATH)
+
+        
+    
+    pbar.set_description('Training   Loss: '+str(loss.item())+string)
+    train_losses.append(loss.item())
