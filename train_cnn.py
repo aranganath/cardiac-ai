8c5189a2d5e451475328997d0bb91cbedf87d96f
Jocelyn Ornelas Munoz <jornelasmunoz@ucmerced.edu>
2024-09-15 16:38:45 -0700

    Loading data Mikels way

--- train_cnn.py
+++ train_cnn.py
@@ -21,21 +21,47 @@ generator = torch.Generator(device=device)
 torch.set_default_dtype(torch.float64)
 
 # Load data
-path = './intracardiac_dataset/'
-VmTrainData, pECGTrainData, VmDataTest, pECGTestData, actTimeTrain, actTimeTest  = fileReader(path, 16000, 0.8)
-print('Data loading from files - complete')
+# Reading file pairs -- Mikel's way
+data_paths=[]
+for dir in os.listdir('./intracardiac_dataset/'):
+    if "data_hearts_dd_0p2" in dir:
+        data_paths.append('./intracardiac_dataset/'+ dir)   
+        
+# datapaths_train = './intracardiac_dataset/data_hearts_dd_0p2'
+file_pairs = read_data_dirs(data_paths)
+data_scaling_ecg = 'normalized_unit'
+data_scaling_vm = 'normalization'
+num_timesteps = 500
+all_channels = True
+input_dim = 12
+output_dim = 75
+outLead = [i for i in range(output_dim)]
+train_len = 12000
+val_len = 4000
 
-VmTrainData = (VmTrainData - torch.min(VmTrainData))/(torch.max(VmTrainData) - torch.min(VmTrainData))
-pECGTrainData = (pECGTrainData - torch.min(pECGTrainData))/(torch.max(pECGTrainData) - torch.min(pECGTrainData))
+# Load training and validation sets normalized
+TrainData = Ecg2VmDataset(file_pairs[:train_len], outLead, num_timesteps, 
+              scaling_ecg= data_scaling_ecg, scaling_vm= data_scaling_vm, 
+              noise_ecg= 'none', noise_vm= 'none')
+ValData = Ecg2VmDataset(file_pairs[train_len:train_len+val_len], outLead, num_timesteps, 
+              scaling_ecg= data_scaling_ecg, scaling_vm= data_scaling_vm, 
+              noise_ecg= 'none', noise_vm= 'none')
 
-VmDataTest = (VmDataTest - torch.min(VmDataTest))/(torch.max(VmDataTest) - torch.min(VmDataTest))
+# path = './intracardiac_dataset/'
+# VmTrainData, pECGTrainData, VmDataTest, pECGTestData, actTimeTrain, actTimeTest  = fileReader(path, 16000, 0.8)
+# print('Data loading from files - complete')
 
-pECGTestData = (pECGTestData - torch.min(pECGTestData))/(torch.max(pECGTestData) - torch.min(pECGTestData))
-print('Normalization - complete!')
+# VmTrainData = (VmTrainData - torch.min(VmTrainData))/(torch.max(VmTrainData) - torch.min(VmTrainData))
+# pECGTrainData = (pECGTrainData - torch.min(pECGTrainData))/(torch.max(pECGTrainData) - torch.min(pECGTrainData))
+
+# VmDataTest = (VmDataTest - torch.min(VmDataTest))/(torch.max(VmDataTest) - torch.min(VmDataTest))
+
+# pECGTestData = (pECGTestData - torch.min(pECGTestData))/(torch.max(pECGTestData) - torch.min(pECGTestData))
+# print('Normalization - complete!')
 
 # 80 -10 -10 split 
-TrainData = CNN1dDataset(VmTrainData, pECGTrainData)
-ValData = CNN1dDataset(VmDataTest, pECGTestData)
+# TrainData = CNN1dDataset(VmTrainData, pECGTrainData)
+# ValData = CNN1dDataset(VmDataTest, pECGTestData)
 #TestData = CNN1dDataset(VmDataTest[data_split:], pECGTestData[data_split:])
 
 model = SqueezeNet(version='1_1')
@@ -52,8 +78,11 @@ dropout = 0.0
 # loss_norm = MSE
 load_model = False
 model_path = '/home/jornelasmunoz/cardiac-ai/cnnModel_v1.training.epoch.36.pth'#./mymodel.training.epoch.1400.pth
-#model = torch.load(model_path)
-#model.to(device)
+if load_model:
+    model = torch.load(model_path)
+    model.to(device)
+    print(f"Loading model from {model_path}")
+    
 criterion = torch.nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters())
 scheduler = torch.optim.lr_scheduler.StepLR(optimizer,step_size_scheduler,gamma=gamma_scheduler)
